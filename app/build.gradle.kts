d80a42a87e3dfabf1193ec866975daf906d14095
8023BIAO <49405327+8023BIAO@users.noreply.github.com>
2026-09-24 23:32:01 +0000

    版本号：vc161 → vc166（v2026.09.25-58）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.a10miaomiao.bilimiao.mod"
         minSdk = 24
         targetSdk = 36
-        versionCode = 161
-        versionName = "v2026.09.21-53"
+        versionCode = 166
+        versionName = "v2026.09.25-58"
 
         flavorDimensions("default")
 
